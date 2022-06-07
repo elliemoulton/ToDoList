dc7e5c569d2166be948b96813623d84d6ae03134
delfino <79332808+elysemoulton@users.noreply.github.com>
2022-06-07 15:05:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let items = [];
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+
+  let today = new Date();
+
+  let options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    day: day,
+    newListItems: items
+  });
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+})
